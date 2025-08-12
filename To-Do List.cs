9e445bfa1b6a3d1532e4d8db3502ad40cae2de02
Daniel Barbosa Siebert <145693593+Lilbarbo@users.noreply.github.com>
2025-08-12 15:34:12 -0300

    Update To-Do List.cs

--- To-Do List.cs
+++ To-Do List.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
+using System.Globalization;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public class Task
 {
@@ -11,15 +13,18 @@ public class Task
 
 class Program
 {
+    static List<Task> tasks = new List<Task>();
+
+    static DateTime localNow = DateTime.Now;
     static void Main()
     {
-        List<Task> tasks = new List<Task>();
         int valor = 0;
 
         do
         {
             Console.Clear();
             Console.WriteLine("-------TO-DO LIST-------");
+            Console.WriteLine("Current Local Time: " + localNow);
             Console.WriteLine("\nPressione 1 para adicionar uma nova task");
             Console.WriteLine("Pressione 2 para ver todas suas tasks para hoje");
             Console.WriteLine("Pressione 3 para sair");
@@ -28,67 +33,127 @@ class Program
             switch (valor)
             {
                 case 1:
-                    Console.Clear();
-                    Console.WriteLine("-------Adicionar uma Task-------");
-                    Console.Write("\nDigite o nome de sua nova task: ");
-                    string nomeTask = Console.ReadLine();
-                    if (!String.IsNullOrWhiteSpace(nomeTask))
-                    {
-                        Console.Write("Descreva sua taks (opcional): ");
-                        string descricao = Console.ReadLine();
-                        if (String.IsNullOrWhiteSpace(descricao))
-                        {
-                            descricao = "";
-                        }
+                    AddTask();
 
-                        Task novaTask = new Task();
-                        novaTask.name = nomeTask;
-                        novaTask.description = descricao;
-                        novaTask.resolvida = false; //por padrão as tasks começam não estando resolvidas
-
-                        tasks.Add(novaTask);
-
-                        Console.WriteLine("\nTask adicionada com sucesso!!");
-                        Console.WriteLine("Pressione qualquer tecla para retornar");
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Nome inválido");
-                    }
                     break;
 
                 case 2:
-                    Console.Clear();
-                    Console.WriteLine("-------Tasks para Hoje-------");
-                    
-                    if(tasks.Count > 0)
-                    {
-                        int taskIndex = 1;
+                    SeeAllTasks();
 
-                        foreach (var task in tasks)
-                        {
-                            Console.WriteLine("\n" + taskIndex + ": " + task.name);
-                            Console.WriteLine(task.description);
-                            Console.WriteLine("Taks resolvida? " + task.resolvida);
-                            taskIndex++;
-                        }
+                    break;
 
-                        Console.WriteLine("\nPressione qualquer tecla para retornar");
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nNenhuma taks para hoje");
-
-                        Console.WriteLine("\nPressione qualquer tecla para retornar");
-                        Console.ReadKey();
-                    }
+                case 3:
+                    QuitAplicattion();
 
                     break;
             }
 
         }
         while (valor != 3);
+    }
+
+    static void QuitAplicattion()
+    {
+        Console.Clear();
+        Console.WriteLine("Encerrando...");
+    }
+
+    static void AddTask()
+    {
+        Console.Clear();
+        Console.WriteLine("-------Adicionar uma Task-------");
+        Console.Write("\nDigite o nome de sua nova task: ");
+        string nomeTask = Console.ReadLine();
+        if (!String.IsNullOrWhiteSpace(nomeTask))
+        {
+            Console.Write("Descreva sua taks (opcional): ");
+            string descricao = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(descricao))
+            {
+                descricao = "";
+            }
+
+            Task novaTask = new Task();
+            novaTask.name = nomeTask;
+            novaTask.description = descricao;
+            novaTask.resolvida = false; //por padrão as tasks começam não estando resolvidas
+
+            tasks.Add(novaTask);
+
+            Console.WriteLine("\nTask adicionada com sucesso!!");
+            Console.WriteLine("Pressione qualquer tecla para retornar");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Nome inválido");
+        }
+    }
+
+    static void SeeAllTasks()
+    {
+        Console.Clear();
+        Console.WriteLine("-------Tasks para Hoje-------");
+
+        if (tasks.Count > 0)
+        {
+            int taskIndex = 0;
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine("\n" + taskIndex + ": " + task.name);
+                Console.WriteLine(task.description);
+                Console.WriteLine("Taks resolvida? " + task.resolvida);
+                taskIndex++;
+            }
+
+            int valor;
+            Console.WriteLine("\nPressione 1 para concluir uma task");
+            Console.WriteLine("Pressione qualquer outra tecla para retornar");
+            int.TryParse(Console.ReadLine(), out valor);
+            if(valor == 1)
+            {
+               FinishTask();
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("\nNenhuma taks para hoje");
+
+            Console.WriteLine("\nPressione qualquer tecla para retornar");
+            Console.ReadKey();
+        }
+    }
+
+    static void FinishTask()
+    {
+        int indexTask;
+        Console.WriteLine("\nDigite o numeração da task que você deseja concluir");
+        int.TryParse(Console.ReadLine(), out indexTask);
+
+        var taskConcluida = tasks[indexTask];
+        if (tasks.Count > indexTask && taskConcluida != null)
+        {
+            if (!taskConcluida.resolvida)
+            {
+                taskConcluida.resolvida = true;
+                Console.WriteLine("\nTask " + taskConcluida.name + " concluída com sucesso!");
+                Console.WriteLine("Pressione qualquer tecla para retornar");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nEssa task já foi concluída");
+                Console.WriteLine("Pressione qualquer tecla para retornar");
+                Console.ReadKey();
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nNumeração de task inválida");
+            Console.WriteLine("Pressione qualquer tecla para retornar");
+            Console.ReadKey();
+        }
     }
 }
