758209e26aa41cfad192002247e82e320c9c96a1
Daniel Barbosa Siebert <145693593+Lilbarbo@users.noreply.github.com>
2025-08-12 17:37:39 -0300

    Update To-Do List.cs

--- To-Do List.cs
+++ To-Do List.cs
@@ -2,20 +2,23 @@ using System;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Security.Principal;
 
 public class Task
 {
-    public string name {  get; set; }
+    public string name { get; set; }
     public string description { get; set; }
-    public bool resolvida { get; set; } 
+    public bool resolvida { get; set; }
 
 }
 
 class Program
 {
+    
+
     static List<Task> tasks = new List<Task>();
 
-    static DateTime localNow = DateTime.Now;
+    static DateOnly dataAtual = DateOnly.FromDateTime(DateTime.Now);
     static void Main()
     {
         int valor = 0;
@@ -24,7 +27,7 @@ class Program
         {
             Console.Clear();
             Console.WriteLine("-------TO-DO LIST-------");
-            Console.WriteLine("Current Local Time: " + localNow);
+            Console.WriteLine("-------"+dataAtual.ToString()+"-------");
             Console.WriteLine("\nPressione 1 para adicionar uma nova task");
             Console.WriteLine("Pressione 2 para ver todas suas tasks para hoje");
             Console.WriteLine("Pressione 3 para sair");
@@ -60,34 +63,43 @@ class Program
 
     static void AddTask()
     {
+        bool respostaValida = false;
         Console.Clear();
-        Console.WriteLine("-------Adicionar uma Task-------");
-        Console.Write("\nDigite o nome de sua nova task: ");
-        string nomeTask = Console.ReadLine();
-        if (!String.IsNullOrWhiteSpace(nomeTask))
+
+        do
         {
-            Console.Write("Descreva sua taks (opcional): ");
-            string descricao = Console.ReadLine();
-            if (String.IsNullOrWhiteSpace(descricao))
+            Console.WriteLine("\n-------Adicionar uma Task-------");
+            Console.Write("\nDigite o nome de sua nova task: ");
+            string nomeTask = Console.ReadLine();
+            if (!String.IsNullOrWhiteSpace(nomeTask))
             {
-                descricao = "";
+                Console.Write("Descreva sua taks (opcional): ");
+                string descricao = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(descricao))
+                {
+                    descricao = "";
+                }
+
+                Task novaTask = new Task();
+                novaTask.name = nomeTask;
+                novaTask.description = descricao;
+                novaTask.resolvida = false; //por padrão as tasks começam não estando resolvidas
+
+                tasks.Add(novaTask);
+
+                Console.WriteLine("\nTask adicionada com sucesso!!");
+                Console.WriteLine("Pressione qualquer tecla para retornar");
+                Console.ReadKey();
+
+                respostaValida = true;
+            }
+            else
+            {
+                Console.WriteLine("Nome inválido!");
             }
 
-            Task novaTask = new Task();
-            novaTask.name = nomeTask;
-            novaTask.description = descricao;
-            novaTask.resolvida = false; //por padrão as tasks começam não estando resolvidas
-
-            tasks.Add(novaTask);
-
-            Console.WriteLine("\nTask adicionada com sucesso!!");
-            Console.WriteLine("Pressione qualquer tecla para retornar");
-            Console.ReadKey();
         }
-        else
-        {
-            Console.WriteLine("Nome inválido");
-        }
+        while (!respostaValida);
     }
 
     static void SeeAllTasks()
@@ -103,17 +115,21 @@ class Program
             {
                 Console.WriteLine("\n" + taskIndex + ": " + task.name);
                 Console.WriteLine(task.description);
-                Console.WriteLine("Taks resolvida? " + task.resolvida);
+                Console.WriteLine("Status: Resolvida? " + task.resolvida);
                 taskIndex++;
             }
 
             int valor;
             Console.WriteLine("\nPressione 1 para concluir uma task");
+            Console.WriteLine("Pressione 2 para apagar uma task");
             Console.WriteLine("Pressione qualquer outra tecla para retornar");
             int.TryParse(Console.ReadLine(), out valor);
-            if(valor == 1)
+            switch(valor)
             {
-               FinishTask();
+                case 1:
+                    FinishTask(); break;
+                case 2:
+                    DeleteTask(); break;
             }
 
         }
@@ -132,9 +148,10 @@ class Program
         Console.WriteLine("\nDigite o numeração da task que você deseja concluir");
         int.TryParse(Console.ReadLine(), out indexTask);
 
-        var taskConcluida = tasks[indexTask];
-        if (tasks.Count > indexTask && taskConcluida != null)
+        if (tasks.Count > indexTask && tasks[indexTask] != null)
         {
+            var taskConcluida = tasks[indexTask];
+
             if (!taskConcluida.resolvida)
             {
                 taskConcluida.resolvida = true;
@@ -155,5 +172,33 @@ class Program
             Console.WriteLine("Pressione qualquer tecla para retornar");
             Console.ReadKey();
         }
+    }
+
+    static void DeleteTask()
+    {
+        int taskIndex;
+        Console.WriteLine("\nDigite a numeração da task que você deseja deletar");
+        int.TryParse(Console.ReadLine(), out taskIndex);
+
+        if(tasks.Count > taskIndex && tasks[taskIndex] != null)
+        {
+            var taskDeletada = tasks[taskIndex];
+            Console.WriteLine("\nTask " + taskDeletada.name + " deletada com sucesso");
+            tasks.Remove(taskDeletada);
+            Console.WriteLine("Pressione qualquer tecla para retornar");
+            Console.ReadKey();
+
+        }
+        else
+        {
+            Console.WriteLine("\nNumeração de task inválida");
+            Console.WriteLine("Pressione qualquer tecla para retornar");
+            Console.ReadKey();
+        }
+    }
+
+    static void SaveTask()
+    {
+        //aqui o bagulho vai ficar louco
     }
 }
